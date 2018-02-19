b4b0308e99f8815dde7ae706046889fc328b9f59
Dimitar Dimitrov <dydimitrov86@gmail.com>
2018-02-19 15:20:30 +0200

    Add files via upload

---
+++ StartUp.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using P01_HospitalDatabase.Data;
+using P01_HospitalDatabase.Data.Models;
+
+
+namespace P01_HospitalDatabase
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+
+        }
+    }
+}
